ac8ddc94f584a8efad43f0d3d9923df8d82f2d29
salavat <si300896@gmail.ru>
2016-11-26 20:13:20 +0600

    test

--- main.cpp
+++ main.cpp
@@ -112,9 +112,9 @@ void rgbHistogramm(Mat img) {
     for (int i = 0; i < img.rows; i++){
         for (int k = 0; k < img.cols; k++){
             // функция  round((cdf(v)-cdfmin)/((N*M)-cdfmin)*(L-1))
-            dst.at<Vec3b>(i,k)[2] = round((float)(cdfB[img.at<Vec3b>(i,k)[2]]-cdfB[minIntensB])/(float)((img.rows*img.cols)-cdfB[minIntensB])*255);
+            dst.at<Vec3b>(i,k)[0] = round((float)(cdfB[img.at<Vec3b>(i,k)[0]]-cdfB[minIntensB])/(float)((img.rows*img.cols)-cdfB[minIntensB])*255);
             dst.at<Vec3b>(i,k)[1] = round((float)(cdfG[img.at<Vec3b>(i,k)[1]]-cdfG[minIntensG])/(float)((img.rows*img.cols)-cdfG[minIntensG])*255);
-            dst.at<Vec3b>(i,k)[0] = round((float)(cdfR[img.at<Vec3b>(i,k)[0]]-cdfR[minIntensR])/(float)((img.rows*img.cols)-cdfR[minIntensR])*255);
+            dst.at<Vec3b>(i,k)[2] = round((float)(cdfR[img.at<Vec3b>(i,k)[2]]-cdfR[minIntensR])/(float)((img.rows*img.cols)-cdfR[minIntensR])*255);
         }
     }
 
